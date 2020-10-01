6039e225f4b32816a8cc36b84caf00bc1759b02d
JeanK25 <kevin.serra25@gmail.com>
2020-10-01 15:19:41 +0200

    Modification de la fonction ajoutEnfant, bon format de DIV.

--- js/formView.js
+++ js/formView.js
@@ -62,14 +62,13 @@ function supprChildren(child){
 
 function addChildren(){
 
-    counter++;
+
     const target = document.getElementById("enfants");
     const div = document.createElement('div');
     const divName = 'enfant' + counter;
     div.setAttribute('id', divName);
-    let content = '<div id="marmots">\n' +
-        '    <div class="jumbotron m-3">\n' +
-        '\n' +
+    div.setAttribute('class','jumbotron m-3');
+    let content =
         '  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>\n' +
         '  <hr class="my-4">\n' +
         '  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>\n' +
@@ -84,12 +83,11 @@ function addChildren(){
         '</svg></button>\n' +
         '  <button type="button" onclick="supprChildren(this);" class="btn btn-primary"><svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-pencil" fill="currentColor" xmlns="http://www.w3.org/2000/svg">\n' +
         '  <path fill-rule="evenodd" d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>\n' +
-        '</svg></button>\n' +
-        '</div>\n' +
-        '</div>'
+        '</svg></button>\n';
 
     div.innerHTML = content;
     target.appendChild(div);
+    counter++;
 
 }
 
