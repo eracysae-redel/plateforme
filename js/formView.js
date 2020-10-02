22436ec81c2d74b27764617f06bc3fb45c919a68
JeanK25 <kevin.serra25@gmail.com>
2020-10-02 14:06:43 +0200

    Rajout onClick sur bouton supprimer enfant.

--- js/formView.js
+++ js/formView.js
@@ -77,7 +77,7 @@ function addChildren(){
         '                  <h5>Enfant</h5>\n' +
         '                </div>\n' +
         '                <div class="col-1">\n' +
-        '                    <svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-x-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">\n' +
+        '                    <svg onclick="supprChildren(this)" width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-x-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">\n' +
         '                    <path fill-rule="evenodd" d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>\n' +
         '                    <path fill-rule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>\n' +
         '                    </svg>\n' +
