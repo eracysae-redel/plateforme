c49c79970ba107d70837d2a4ea7125cd7818ce9d
JeanK25 <kevin.serra25@gmail.com>
2020-10-08 16:29:11 +0200

    Ajout d'une classe au click d'un bouton.

--- js/envoiPatri.js
+++ js/envoiPatri.js
@@ -6,7 +6,7 @@ function selectButton () {
            for (let j = 0; j<buttons.length; j++) {
                if(i == j){
                    buttons[j].setAttribute('id','testButton');
-                   buttons[j].setAttribute('class','choisi');
+                   buttons[j].setAttribute('class','choisi btn btn-outline-primary');
                } else {
                    buttons[j].removeAttribute('id');
                    buttons[j].setAttribute('class','btn btn-outline-primary');
