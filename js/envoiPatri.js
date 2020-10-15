85ec041b9d3152e0ccee80ff7a74b6e70878d493
JeanK25 <kevin.serra25@gmail.com>
2020-10-15 19:24:50 +0200

    Fonction d'ajout des données du formulaire de situation patrimoniale à la base de données. Test OK.

--- js/envoiPatri.js
+++ js/envoiPatri.js
@@ -1,153 +1,43 @@
-let revenuAnnuel;
-const revenu1 = document.getElementById('revenu1');
-const revenu2 = document.getElementById('revenu2');
-const revenu3 = document.getElementById('revenu3');
-const revenu4 = document.getElementById('revenu4');
+function writeUserPatri () {
 
-revenu1.addEventListener('click', function () {
-    revenuAnnuel = revenu1;
-    revenuAnnuel.setAttribute('class','choisi btn btn-outline-primary');
-    revenu2.setAttribute('class','btn btn-outline-primary');
-    revenu3.setAttribute('class','btn btn-outline-primary');
-    revenu4.setAttribute('class','btn btn-outline-primary');
-});
-revenu2.addEventListener('click', function () {
-    revenuAnnuel = revenu2;
-    revenuAnnuel.setAttribute('class','choisi btn btn-outline-primary');
-    revenu1.setAttribute('class','btn btn-outline-primary');
-    revenu3.setAttribute('class','btn btn-outline-primary');
-    revenu4.setAttribute('class','btn btn-outline-primary');
-});
-revenu3.addEventListener('click', function () {
-    revenuAnnuel = revenu3;
-    revenuAnnuel.setAttribute('class','choisi btn btn-outline-primary');
-    revenu2.setAttribute('class','btn btn-outline-primary');
-    revenu1.setAttribute('class','btn btn-outline-primary');
-    revenu4.setAttribute('class','btn btn-outline-primary');
-});
-revenu4.addEventListener('click', function () {
-    revenuAnnuel = revenu4;
-    revenuAnnuel.setAttribute('class','choisi btn btn-outline-primary');
-    revenu2.setAttribute('class','btn btn-outline-primary');
-    revenu3.setAttribute('class','btn btn-outline-primary');
-    revenu1.setAttribute('class','btn btn-outline-primary');
-});
+    const user = sessionStorage.getItem('userId');
+    db.ref('users/' + user).child('Situation Patrimoniale').set(null);
+    db.ref('users/' + user + '/Situation Patrimoniale').child('Revenus').set({
+        'Revenus annuels' : revenuAnnuel.value
+    });
 
-let patrimoineAnnuel;
-const patrimoine1 = document.getElementById('patrimoine1');
-const patrimoine2 = document.getElementById('patrimoine2');
-const patrimoine3 = document.getElementById('patrimoine3');
-const patrimoine4 = document.getElementById('patrimoine4');
-const patrimoine5 = document.getElementById('patrimoine5');
+    db.ref('users/' + user + '/Situation Patrimoniale').child('Patrimoine').set({
+        'Estimation patrimoine ' : patrimoineAnnuel.value
+    });
 
-patrimoine1.addEventListener('click', function () {
-    patrimoineAnnuel = patrimoine1;
-    patrimoineAnnuel.setAttribute('class','choisi btn btn-outline-primary');
-    patrimoine2.setAttribute('class','btn btn-outline-primary');
-    patrimoine3.setAttribute('class','btn btn-outline-primary');
-    patrimoine4.setAttribute('class','btn btn-outline-primary');
-    patrimoine5.setAttribute('class','btn btn-outline-primary');
-});
+    db.ref('users/' + user + '/Situation Patrimoniale').child('Dettes').set({
+        'Estimation dettes ' : detteAnnuel.value
+    });
 
-patrimoine2.addEventListener('click', function () {
-    patrimoineAnnuel = patrimoine2;
-    patrimoineAnnuel.setAttribute('class','choisi btn btn-outline-primary');
-    patrimoine1.setAttribute('class','btn btn-outline-primary');
-    patrimoine3.setAttribute('class','btn btn-outline-primary');
-    patrimoine4.setAttribute('class','btn btn-outline-primary');
-    patrimoine5.setAttribute('class','btn btn-outline-primary');
-});
+    db.ref('users/' + user + '/Situation Patrimoniale').child('Repartition Patrimoine').set({
+        'Actifs financiers' : document.getElementById('charge1').value + '%',
+        'Actifs immobilliers' : document.getElementById('charge2').value + '%',
+        'Actifs professionnels' : document.getElementById('charge3').value + '%',
+        'Autres' : document.getElementById('charge4').value + '%'
+    });
 
-patrimoine3.addEventListener('click', function () {
-    patrimoineAnnuel = patrimoine3;
-    patrimoineAnnuel.setAttribute('class','choisi btn btn-outline-primary');
-    patrimoine2.setAttribute('class','btn btn-outline-primary');
-    patrimoine1.setAttribute('class','btn btn-outline-primary');
-    patrimoine4.setAttribute('class','btn btn-outline-primary');
-    patrimoine5.setAttribute('class','btn btn-outline-primary');
-});
+    db.ref('users/' + user + '/Situation Patrimoniale').child('IFI').set({
+        'Redevable IFI' : ifi.value
+    });
 
-patrimoine4.addEventListener('click', function () {
-    patrimoineAnnuel = patrimoine4;
-    patrimoineAnnuel.setAttribute('class','choisi btn btn-outline-primary');
-    patrimoine2.setAttribute('class','btn btn-outline-primary');
-    patrimoine3.setAttribute('class','btn btn-outline-primary');
-    patrimoine1.setAttribute('class','btn btn-outline-primary');
-    patrimoine5.setAttribute('class','btn btn-outline-primary');
-});
+    db.ref('users/' + user + '/Situation Patrimoniale').child('Charges').set({
+        'Charges par mois' : document.getElementById('chargeEngagement').value + '€'
+    });
 
-patrimoine5.addEventListener('click', function () {
-    patrimoineAnnuel = patrimoine5;
-    patrimoineAnnuel.setAttribute('class','choisi btn btn-outline-primary');
-    patrimoine2.setAttribute('class','btn btn-outline-primary');
-    patrimoine3.setAttribute('class','btn btn-outline-primary');
-    patrimoine1.setAttribute('class','btn btn-outline-primary');
-    patrimoine4.setAttribute('class','btn btn-outline-primary');
-});
+    db.ref('users/' + user + '/Situation Patrimoniale').child('Capacité Epargne').set({
+        'Capacité épargne par mois ' : document.getElementById('capaciteEpargne').value + '€'
+    });
+}
 
-let detteAnnuel;
-const dette1 = document.getElementById('dette1');
-const dette2 = document.getElementById('dette2');
-const dette3 = document.getElementById('dette3');
-const dette4 = document.getElementById('dette4');
-const dette5 = document.getElementById('dette5');
 
-dette1.addEventListener('click', function () {
-   detteAnnuel = dette1;
-   detteAnnuel.setAttribute('class','choisi btn btn-outline-primary');
-   dette2.setAttribute('class','btn btn-outline-primary');
-   dette3.setAttribute('class','btn btn-outline-primary');
-   dette4.setAttribute('class','btn btn-outline-primary');
-   dette5.setAttribute('class','btn btn-outline-primary');
-});
 
-dette2.addEventListener('click', function () {
-   detteAnnuel = dette2;
-   detteAnnuel.setAttribute('class','choisi btn btn-outline-primary');
-   dette1.setAttribute('class','btn btn-outline-primary');
-   dette3.setAttribute('class','btn btn-outline-primary');
-   dette4.setAttribute('class','btn btn-outline-primary');
-   dette5.setAttribute('class','btn btn-outline-primary');
-});
-
-dette3.addEventListener('click', function () {
-   detteAnnuel = dette3;
-   detteAnnuel.setAttribute('class','choisi btn btn-outline-primary');
-   dette2.setAttribute('class','btn btn-outline-primary');
-   dette1.setAttribute('class','btn btn-outline-primary');
-   dette4.setAttribute('class','btn btn-outline-primary');
-   dette5.setAttribute('class','btn btn-outline-primary');
-});
-
-dette4.addEventListener('click', function () {
-   detteAnnuel = dette4;
-   detteAnnuel.setAttribute('class','choisi btn btn-outline-primary');
-   dette2.setAttribute('class','btn btn-outline-primary');
-   dette3.setAttribute('class','btn btn-outline-primary');
-   dette1.setAttribute('class','btn btn-outline-primary');
-   dette5.setAttribute('class','btn btn-outline-primary');
-});
-
-dette5.addEventListener('click', function () {
-   detteAnnuel = dette5;
-   detteAnnuel.setAttribute('class','choisi btn btn-outline-primary');
-   dette2.setAttribute('class','btn btn-outline-primary');
-   dette3.setAttribute('class','btn btn-outline-primary');
-   dette4.setAttribute('class','btn btn-outline-primary');
-   dette1.setAttribute('class','btn btn-outline-primary');
-});
-
-let ifi;
-const ifi1 = document.getElementById('ifi1');
-const ifi2 = document.getElementById('ifi2');
-
-ifi1.addEventListener('click', function () {
-    ifi = ifi1;
-    ifi.setAttribute('class','choisi btn btn-outline-primary');
-    ifi2.setAttribute('class','btn btn-outline-primary');
-});
-ifi2.addEventListener('click', function () {
-    ifi = ifi2;
-    ifi.setAttribute('class','choisi btn btn-outline-primary');
-    ifi1.setAttribute('class','btn btn-outline-primary');
+const submitPatri = document.getElementById('submitPatri');
+submitPatri.addEventListener('click', function (e) {
+   e.preventDefault();
+   writeUserPatri();
 });
--- js/formView.js
+++ js/formView.js
@@ -223,3 +223,173 @@ toggleConjoint.addEventListener('change',function (){
         document.getElementById('conjointEntreprise').setAttribute('hidden',true);
     }
 });
+
+let revenuAnnuel;
+const revenu1 = document.getElementById('revenu1');
+const revenu2 = document.getElementById('revenu2');
+const revenu3 = document.getElementById('revenu3');
+const revenu4 = document.getElementById('revenu4');
+
+revenu1.addEventListener('click', function () {
+    revenuAnnuel = revenu1;
+    revenuAnnuel.setAttribute('class','choisi btn btn-outline-primary');
+    revenu2.setAttribute('class','btn btn-outline-primary');
+    revenu3.setAttribute('class','btn btn-outline-primary');
+    revenu4.setAttribute('class','btn btn-outline-primary');
+});
+revenu2.addEventListener('click', function () {
+    revenuAnnuel = revenu2;
+    revenuAnnuel.setAttribute('class','choisi btn btn-outline-primary');
+    revenu1.setAttribute('class','btn btn-outline-primary');
+    revenu3.setAttribute('class','btn btn-outline-primary');
+    revenu4.setAttribute('class','btn btn-outline-primary');
+});
+revenu3.addEventListener('click', function () {
+    revenuAnnuel = revenu3;
+    revenuAnnuel.setAttribute('class','choisi btn btn-outline-primary');
+    revenu2.setAttribute('class','btn btn-outline-primary');
+    revenu1.setAttribute('class','btn btn-outline-primary');
+    revenu4.setAttribute('class','btn btn-outline-primary');
+});
+revenu4.addEventListener('click', function () {
+    revenuAnnuel = revenu4;
+    revenuAnnuel.setAttribute('class','choisi btn btn-outline-primary');
+    revenu2.setAttribute('class','btn btn-outline-primary');
+    revenu3.setAttribute('class','btn btn-outline-primary');
+    revenu1.setAttribute('class','btn btn-outline-primary');
+});
+
+let patrimoineAnnuel;
+const patrimoine1 = document.getElementById('patrimoine1');
+const patrimoine2 = document.getElementById('patrimoine2');
+const patrimoine3 = document.getElementById('patrimoine3');
+const patrimoine4 = document.getElementById('patrimoine4');
+const patrimoine5 = document.getElementById('patrimoine5');
+
+patrimoine1.addEventListener('click', function () {
+    patrimoineAnnuel = patrimoine1;
+    patrimoineAnnuel.setAttribute('class','choisi btn btn-outline-primary');
+    patrimoine2.setAttribute('class','btn btn-outline-primary');
+    patrimoine3.setAttribute('class','btn btn-outline-primary');
+    patrimoine4.setAttribute('class','btn btn-outline-primary');
+    patrimoine5.setAttribute('class','btn btn-outline-primary');
+});
+
+patrimoine2.addEventListener('click', function () {
+    patrimoineAnnuel = patrimoine2;
+    patrimoineAnnuel.setAttribute('class','choisi btn btn-outline-primary');
+    patrimoine1.setAttribute('class','btn btn-outline-primary');
+    patrimoine3.setAttribute('class','btn btn-outline-primary');
+    patrimoine4.setAttribute('class','btn btn-outline-primary');
+    patrimoine5.setAttribute('class','btn btn-outline-primary');
+});
+
+patrimoine3.addEventListener('click', function () {
+    patrimoineAnnuel = patrimoine3;
+    patrimoineAnnuel.setAttribute('class','choisi btn btn-outline-primary');
+    patrimoine2.setAttribute('class','btn btn-outline-primary');
+    patrimoine1.setAttribute('class','btn btn-outline-primary');
+    patrimoine4.setAttribute('class','btn btn-outline-primary');
+    patrimoine5.setAttribute('class','btn btn-outline-primary');
+});
+
+patrimoine4.addEventListener('click', function () {
+    patrimoineAnnuel = patrimoine4;
+    patrimoineAnnuel.setAttribute('class','choisi btn btn-outline-primary');
+    patrimoine2.setAttribute('class','btn btn-outline-primary');
+    patrimoine3.setAttribute('class','btn btn-outline-primary');
+    patrimoine1.setAttribute('class','btn btn-outline-primary');
+    patrimoine5.setAttribute('class','btn btn-outline-primary');
+});
+
+patrimoine5.addEventListener('click', function () {
+    patrimoineAnnuel = patrimoine5;
+    patrimoineAnnuel.setAttribute('class','choisi btn btn-outline-primary');
+    patrimoine2.setAttribute('class','btn btn-outline-primary');
+    patrimoine3.setAttribute('class','btn btn-outline-primary');
+    patrimoine1.setAttribute('class','btn btn-outline-primary');
+    patrimoine4.setAttribute('class','btn btn-outline-primary');
+});
+
+let detteAnnuel;
+const dette1 = document.getElementById('dette1');
+const dette2 = document.getElementById('dette2');
+const dette3 = document.getElementById('dette3');
+const dette4 = document.getElementById('dette4');
+const dette5 = document.getElementById('dette5');
+
+dette1.addEventListener('click', function () {
+    detteAnnuel = dette1;
+    detteAnnuel.setAttribute('class','choisi btn btn-outline-primary');
+    dette2.setAttribute('class','btn btn-outline-primary');
+    dette3.setAttribute('class','btn btn-outline-primary');
+    dette4.setAttribute('class','btn btn-outline-primary');
+    dette5.setAttribute('class','btn btn-outline-primary');
+});
+
+dette2.addEventListener('click', function () {
+    detteAnnuel = dette2;
+    detteAnnuel.setAttribute('class','choisi btn btn-outline-primary');
+    dette1.setAttribute('class','btn btn-outline-primary');
+    dette3.setAttribute('class','btn btn-outline-primary');
+    dette4.setAttribute('class','btn btn-outline-primary');
+    dette5.setAttribute('class','btn btn-outline-primary');
+});
+
+dette3.addEventListener('click', function () {
+    detteAnnuel = dette3;
+    detteAnnuel.setAttribute('class','choisi btn btn-outline-primary');
+    dette2.setAttribute('class','btn btn-outline-primary');
+    dette1.setAttribute('class','btn btn-outline-primary');
+    dette4.setAttribute('class','btn btn-outline-primary');
+    dette5.setAttribute('class','btn btn-outline-primary');
+});
+
+dette4.addEventListener('click', function () {
+    detteAnnuel = dette4;
+    detteAnnuel.setAttribute('class','choisi btn btn-outline-primary');
+    dette2.setAttribute('class','btn btn-outline-primary');
+    dette3.setAttribute('class','btn btn-outline-primary');
+    dette1.setAttribute('class','btn btn-outline-primary');
+    dette5.setAttribute('class','btn btn-outline-primary');
+});
+
+dette5.addEventListener('click', function () {
+    detteAnnuel = dette5;
+    detteAnnuel.setAttribute('class','choisi btn btn-outline-primary');
+    dette2.setAttribute('class','btn btn-outline-primary');
+    dette3.setAttribute('class','btn btn-outline-primary');
+    dette4.setAttribute('class','btn btn-outline-primary');
+    dette1.setAttribute('class','btn btn-outline-primary');
+});
+
+let ifi;
+const ifi1 = document.getElementById('ifi1');
+const ifi2 = document.getElementById('ifi2');
+
+ifi1.addEventListener('click', function () {
+    ifi = ifi1;
+    ifi.setAttribute('class','choisi btn btn-outline-primary');
+    ifi2.setAttribute('class','btn btn-outline-primary');
+});
+ifi2.addEventListener('click', function () {
+    ifi = ifi2;
+    ifi.setAttribute('class','choisi btn btn-outline-primary');
+    ifi1.setAttribute('class','btn btn-outline-primary');
+});
+
+const baseRepartition = 100;
+let repartition = 100;
+let sumCpt = 0;
+let cpts = [ cpt1 = 0, cpt2 = 0, cpt3 = 0, cpt4 = 0];
+for (let i = 0 ; i < cpts.length ; i++) {
+    document.getElementById('charge' + (i+1)).addEventListener('change', function (e) {
+        cpts[i] = e.target.value;
+        repartition = baseRepartition - cpts[0] - cpts[1] - cpts[2] - cpts[3];
+        if(repartition === 0) {
+            document.getElementById('submitPatri').removeAttribute('disabled');
+        } else {
+            document.getElementById('submitPatri').setAttribute('disabled', 'true');
+        }
+    });
+}
